3bea519df3bca43ac53056a027b2373355f8be23
Anastasiya-Vavilova <ntttru@bk.ru>
2025-01-16 22:57:48 +0300

    Dorabotks

--- gh.test.js
+++ gh.test.js
@@ -1,13 +1,20 @@
 let page;
 
+jest.setTimeout(60_000);
+
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
+
 });
 
 afterEach(() => {
   page.close();
 });
+
+describe("Github page tests", () => {
+  beforeEach(async () => {
+    await page.goto("https://github.com/team");
+  });
 
   test("The h1 header content'", async () => {
     const expected =
@@ -20,14 +27,14 @@ afterEach(() => {
     expect(title2).toEqual(expected);
   });
 
- test("The first link attribute", async () => {
+  test("The first link attribute", async () => {
     const expected = "#start-of-content";
     page.setDefaultTimeout(5_000);
     const actual = await page.$eval("a", (link) => link.getAttribute("href"));
     expect(actual).toEqual(expected);
   });
 
-    test("The page contains Sign in button", async () => {
+  test("The page contains Sign in button", async () => {
     const expected = "Get started with Team";
     page.setDefaultTimeout(5_000);
     const btnSelector = ".btn-large-mktg.btn-mktg";
@@ -37,14 +44,51 @@ afterEach(() => {
     const actual = await page.$eval(btnSelector, (link) => link.textContent);
     expect(actual).toContain(expected);
   });
+});
 
-  test("The page contains Sign in button", async () => {
-    const btnSelector = ".btn-large-mktg.btn-mktg";
-    await page.waitForSelector(btnSelector, {
-      visible: true,
-    });
-    const actual = await page.$eval(btnSelector, link => link.textContent);
-    expect(actual).toContain("Sign up for free")
+describe("Testing the Solutions menu", () => {
+  beforeEach(async () => {
+    await page.goto("https://github.com");
+    page.setDefaultTimeout(20_000);
   });
 
+  test("1. Go to page Enterprise", async () => {
+    const expected = "The AI Powered Developer Platform. · GitHub";
 
+    await page.hover("li:nth-child(2) button:nth-child(1)"); //наводит курсор и открывает всплывающее окно
+    const firstLink = await page.$("a[href='https://github.com/enterprise']");
+    await firstLink.click();
+    await page.waitForNavigation(); //Ждём смены страницы
+    const title = await page.title();
+
+    expect(title).toEqual(expected);
+  });
+
+  test("2. Go to page Startups", async () => {
+    const expected =
+      "GitHub for Startups: Build your startup on GitHub · GitHub";
+
+    await page.hover("li:nth-child(2) button:nth-child(1)"); //наводит курсор и открывает всплывающее окно
+    const firstLink = await page.$(
+      "a[href='https://github.com/enterprise/startups']"
+    );
+    await firstLink.click();
+    await page.waitForNavigation(); //Ждём смены страницы
+
+    const title = await page.title();
+    expect(title).toEqual(expected);
+  });
+
+  test("3. Go to page DevOps", async () => {
+    const expected =
+      "Unified AI-Powered Platforms for DevOps Solutions | GitHub · GitHub";
+
+    await page.hover("li:nth-child(2) button:nth-child(1)"); //наводит курсор и открывает всплывающее окно
+    const firstLink = await page.$("a[href='/solutions/use-case/devops']");
+    await firstLink.click();
+    await page.waitForNavigation(); //Ждём смены страницы
+    const title = await page.title();
+
+    expect(title).toEqual(expected);
+  });
+});
--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-    launch: {
-        headless: false,
-        slowMo: 2000,
-        defaultViewport: null,
-        args: ['--start-maximized'] //— используем максимальный размер окна браузера
-      },
-    
-  };
-  
+  launch: {
+    headless: false,
+    slowMo: 150, // задержка между действиями (без неё проходит 1 сьют из ~10)
+    devtools: false,
+    defaultViewport: null,
+    args: ["--start-maximized"], //— используем максимальный размер окна браузера
+  },
+};
